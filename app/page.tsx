7ec8a45ce87af329eb8a9f06006008a85086ba61
Ranjeet Vishwakarma <ranjeetv.dev@gmail.com>
2026-03-13 15:29:16 +0530

    Impleted the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
+import HeroSection from "@/components/HeroSection";
+import BookCard from "@/components/BookCard";
+import { sampleBooks } from "@/lib/constants";
+import Search from "@/components/Search";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="wrapper container">
+      <HeroSection />
+
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
+        <h2 className="text-3xl font-serif font-bold text-[#212a3b]">
+          Recent Books
+        </h2>
+        <Search />
+      </div>
+
+      <div className="library-books-grid">
+        {sampleBooks.map(({ _id, title, author, coverURL, slug }) => (
+          <BookCard
+            key={_id}
+            title={title}
+            author={author}
+            coverURL={coverURL}
+            slug={slug}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
